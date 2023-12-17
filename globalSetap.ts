3ed04ed52721141b2982988441bd6847810948c4
DubovikovArtem <dubovikov.arm@gmail.com>
2023-12-17 16:34:56 +0200

    add playwright

---
+++ globalSetap.ts
@@ -0,0 +1,5 @@
+async function globalSetup() {
+    console.log("globalSetup");
+}
+
+export default globalSetup;
---
+++ globalTeardown.ts
@@ -0,0 +1,5 @@
+async function globalTeardown() {
+    console.log("globalTeardown");
+}
+
+export default globalTeardown;
